17eaf81ff71660412276f088590fd0fcb990d179
mehdi0-0s <mehdii84sl@gmail.com>
2025-04-19 22:04:10 +0330

    Revert "Second Commit"
    This reverts commit 14666aeb53dee44d707359560b7559e1d495a078.

--- main.cpp
+++ main.cpp
@@ -6,14 +6,8 @@ void print_hello()
     cout << "Hello, World" << endl;
 }
 
-
-void print_studentNum(){
-    cout << "student number: 1234"<<endl;
-}
-
 int main()
 {
     print_hello();
-    print_studentNum();
     return 0;
 }
